1fefe80226b76bb2dc934ff2604d745b0e1d09fb
Jean Perbet <jeanperbet@icloud.com>
2024-01-26 11:04:23 +0100

    issue #1 - fix code skeleton/add .gitignore file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,15 @@
-package src;
+package main;
 import java.awt.geom.Point2D;
-
 
 public final class Main {
 
-    // CMV : fill with the logical value of each LIC 
-    // add the rest of the parameters we may need such as CMV 
+    // main.CMV : fill with the logical value of each LIC
+    // add the rest of the parameters we may need such as main.CMV
     public int NUMPOINTS;
     public Point2D[] POINTS; 
     
     // add other relevant parameters 
     public static void main(String[] args) {
-        Parameters parameters = new Parameters(); 
-        parameters.LENGTH1 = 10.0;
     
         System.out.println("NO");
     }
