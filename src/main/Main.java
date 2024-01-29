200301fea994316aaa781016a8bc7943dbf652b0
Zaina-ram <zainaramadan74@gmail.com>
2024-01-29 21:21:21 +0100

    issue #2 #3 #4 - fix: fixed values for parameters to pass test

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,32 +1,8 @@
-/*package main;
-import java.awt.geom.Point2D;
 
-public final class Main {
-
-    public static int numPoints;
-    public static Point2D[] points;
-    
-    
-    // add other relevant parameters 
-    public static void main(String[] args) {
-        numPoints  = 5;
-        Parameters parameters = new Parameters();
-        Point2D[] points = new Point2D[numPoints];
-        points[0] = new Point2D.Double(0, 0);
-        points[1] = new Point2D.Double(1, 0);
-        points[2] = new Point2D.Double(1, 0);
-        points[3] = new Point2D.Double(2, 2);
-        points[4] = new Point2D.Double(3, 0);
-
-        CMV cmv = new CMV(5,points, parameters);
-        cmv.setLIC2();
-       // System.out.println("NO");
-    }
-}*/
 package main;
 import java.awt.geom.Point2D;
 
-import org.junit.Test;
+
 
 public final class Main {
 
