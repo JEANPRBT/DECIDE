ec3104452e0604c763e183047280e266a0dc48c1
Raahitya <raahitya509@gmail.com>
2024-01-29 20:20:58 +0100

    issue #16-feat: added LIC16 and its tests

--- src/main/CMV.java
+++ src/main/CMV.java
@@ -26,6 +26,8 @@ public final class CMV {
         setLIC3();
         setLIC4();
         setLIC5();
+        setLIC12();
+        setLIC13();
     }
 
     /**
@@ -92,6 +94,81 @@ public final class CMV {
         }
     }
 
+    /**
+     * Setter for the LIC n°12.
+     * There exists at least one set of two data points, separated by exactly K PTS consecutive intervening points, which are a distance greater than the length, LENGTH1, apart.
+     * In addi- tion, there exists at least one set of two data points (which can be the same or different from the two data points just mentioned), separated by exactly K PTS consecutive intervening points, that are a distance less than the length, LENGTH2, apart.
+     * Both parts must be true for the LIC to be true. The condition is not met when NUMPOINTS < 3.
+     * 0 ≤ LENGTH2
+     */
+    public void setLIC12(){
+        boolean c_1 = false, c_2= false;  //c_1/2= conditions 
+        int k_pts = this.parameters.k_pts;
+        if (numPoints < k_pts + 2){
+            return;
+        }
+        for (int i = 0; i< numPoints - k_pts - 1; i++){
+            Point2D p1 = this.points[i], p2 = this.points[i + k_pts + 1];
+            double distance = p1.distance(p2);
+            if (distance > this.parameters.length1) {
+                c_1 = true;
+            }
+            if (distance < this.parameters.length2) {
+                c_2 = true;
+            }
+            if (c_1 && c_2){
+                this.cmv[12] = true;
+                break;
+            }
+        }
+    }
+
+    /*
+        Setter for the LIC n°13.
+        There exists at least one set of three data points, separated by exactly A PTS and B PTS
+        consecutive intervening points, respectively, that cannot be contained within or on a circle of
+        radius RADIUS1. In addition, there exists at least one set of three data points (which can be
+        the same or different from the three data points just mentioned) separated by exactly A PTS
+        and B PTS consecutive intervening points, respectively, that can be contained in or on a
+        circle of radius RADIUS2. Both parts must be true for the LIC to be true. The condition is
+        not met when NUMPOINTS < 5.
+        0 ≤ RADIUS2
+     */
+    
+    public void setLIC13(){
+        this.cmv[13]= false;
+        if (numPoints<5){ //condition is not met
+            return;
+        }
+        for(int i=0; i <= numPoints-3; i++){
+            int a_index = i + 1 + this.parameters.a_pts;
+            int b_index = a_index + 1 + this.parameters.b_pts;
+
+            if (b_index < numPoints){
+                Point2D  pointA = this.points[i];
+                Point2D  pointB = this.points[a_index];
+                Point2D  pointC = this.points[b_index];
+                boolean c1 = !pointsInCircle(pointA,pointB,pointC, this.parameters.radius1);
+                boolean c2 = pointsInCircle(pointA,pointB,pointC,this.parameters.radius2);
+                if (c1 && c2){
+                    this.cmv[13] = true;
+                    break;
+                }
+            }
+        }   
+    }
+    private boolean pointsInCircle(Point2D pointA, Point2D pointB,  Point2D pointC, double radius){
+        double l_a = pointA.distance(pointB); // lengths of sides of triangle
+        double l_b =  pointB.distance(pointC);
+        double l_c = pointC.distance(pointA);
+        double s = (l_a+l_b+l_c)/2;
+        if (l_a + l_b == l_c || l_a + l_c == l_b || l_b + l_c == l_a) {
+            return false; // Collinear points cannot form a circumcircle with finite radius
+        }
+        double area = Math.sqrt((s - l_a)*(s - l_b)*(s - l_c)); // area of  triangle
+        double circum_radius= (l_a*l_b*l_c)/(4*area);
+        return circum_radius <= radius; // checks if c_r is less than or equal to given radius
+    }
     /**
      * Getter for CMV.
      * @return the boolean array of all LICs representing the CMV.
--- src/test/CMVTest.java
+++ src/test/CMVTest.java
@@ -7,6 +7,7 @@ public class CMVTest {
     int numPoints = 5;
     Point2D[] points = new Point2D[numPoints];
     Parameters parameters = new Parameters();
+   
     @Test
     public void LIC3PositiveTest(){
         points[0] = new Point2D.Double(0, 0);
@@ -71,4 +72,60 @@ public class CMVTest {
         CMV cmv = new CMV(numPoints, points, parameters);
         assertFalse(cmv.getCMV()[5]);
     }
+    @Test
+    public void LIC12PositiveTest() {
+        parameters.k_pts = 1;  
+        parameters.length1 = 2.0;  
+        parameters.length2 = 1.0;  
+        points[0] = new Point2D.Double(0, 0);
+        points[1] = new Point2D.Double(0.5, 0); 
+        points[2] = new Point2D.Double(3,0);  
+        points[3] = new Point2D.Double(1, 0); 
+        points[4] = new Point2D.Double(2, 0);
+        CMV cmv = new CMV(numPoints, points, parameters);
+        assertTrue(cmv.getCMV()[12]);
+    }
+    @Test
+    public void LIC12NegativeTest() {
+        parameters.k_pts = 1;  
+        parameters.length1 = 2.0;  
+        parameters.length2 = 1.0;  
+        points[0] = new Point2D.Double(0, 0);
+        points[1] = new Point2D.Double(1.5, 0); 
+        points[2] = new Point2D.Double(2, 0);   
+        points[3] = new Point2D.Double(3, 0);   
+        points[4] = new Point2D.Double(4, 0);  
+        CMV cmv = new CMV(numPoints, points, parameters);
+        assertFalse(cmv.getCMV()[12]);  
+    }
+    @Test
+    public void LIC13PositiveTest() {
+        parameters.a_pts = 1;
+        parameters.b_pts = 1;
+        parameters.radius1 = 1.0; 
+        parameters.radius2 = 10.0;
+        points[0] = new Point2D.Double(0, 0); 
+        points[1] = new Point2D.Double(2, 0); 
+        points[2] = new Point2D.Double(4, 0); 
+        points[3] = new Point2D.Double(1, 1); 
+        points[4] = new Point2D.Double(3, 1); 
+        CMV cmv = new CMV(numPoints, points, parameters);
+        assertTrue(cmv.getCMV()[13]);
+    }
+
+    @Test
+    public void LIC13NegativeTest() {
+    parameters.a_pts = 1;
+    parameters.b_pts = 1;
+    parameters.radius1 = 5.0; 
+    parameters.radius2 = 2.0; 
+    points[0] = new Point2D.Double(0, 0);
+    points[1] = new Point2D.Double(1, 1); 
+    points[2] = new Point2D.Double(2, 2); 
+    points[3] = new Point2D.Double(3, 3); 
+    points[4] = new Point2D.Double(4, 4); 
+    CMV cmv = new CMV(numPoints, points, parameters);
+    assertFalse(cmv.getCMV()[13]);
+    }
+
 }
